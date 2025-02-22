38f10809b1ea119c39196e399b496e71712d9772
Flavio Saldaña <flaviogaston88@gmail.com>
2025-02-22 11:02:01 -0300

    modificaciónes en app.js

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
-console.log('hola');
+let amigos = [];
+
+const agregarAmigo = () => {
+    let nombre = document.getElementById('amigo');
+    nombre.value != '' ? amigos.push(nombre.value) : alert('Ingrese un nombre valido');
+    console.log(nombre.value);
+    nombre.value = '';
+
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    
+    for(let i = 0; i < amigos.length; i++){
+        let li = document.createElement('li');
+        li.innerText = amigos[i];
+        lista.appendChild(li);
+    }
+}
+
+const sortearAmigo = () => {
+    if(amigos.length){
+        let numRandom = Math.floor(Math.random() * (amigos.length));
+        let resultado = document.getElementById('resultado');
+        resultado.innerHTML = amigos[numRandom];
+    }
+}
